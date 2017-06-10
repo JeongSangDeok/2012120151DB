1171fe97044239c0ea25fdb4d88ecf111c897e46
JeongSangdeok <tkdejr1002@naver.com>
2017-06-10 12:13:19 +0000

    Login Page

---
+++ Login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8">
+<title>LOGIN</title>
+</head>
+<body>
+
+  <div style = "text-align:center;padding-top:15px">
+	<br>
+    <span style = "font-size:30px">LOGIN PAGE</span>
+	<br>
+	<br>
+  </div>
+  
+  <form>
+ <div style="text-align:center">   
+	 ID :	 
+	 <input type="text" name="id" size="25" placeholder="ID입력"/> 
+     <br>
+     PW : <input type ="password" size ="25" name ="pwd" /> <br>
+     <br>
+	 <input type = "submit" value = "LOGIN" />
+	 <br>
+	 <br>
+	 <a href = "main.html">뒤로 가기(BACK)</a>
+	
+ </div>
+  </form>
+ 
+</body>
+
+</html>
