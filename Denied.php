c9902b3bf67b57b7360988afabeb85fc32203440
JeongSangdeok <tkdejr1002@naver.com>
2017-06-10 12:11:45 +0000

    Main Page

---
+++ Denied.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8">
+<title>DENIED</title>
+</head>
+<body>
+
+  <div style = "text-align:center;padding-top:15px">
+	<br>
+    <span style = "font-size:30px">권한 없음</span>
+	<br>
+	<br>
+  </div>
+  
+  <form>
+ <div style="text-align:center">   
+	 <h1>로그인을 먼저 해주세요</h1>	
+	 <br>
+	 <a href = "Init.html">뒤로 가기(BACK)</a>
+	
+ </div>
+  </form>
+ 
+</body>
+
+</html>
